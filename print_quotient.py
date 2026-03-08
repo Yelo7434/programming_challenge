f526afc79921e89529968da81a3d8cc00b550194
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-09 00:05:45 +0800

    print quotient.py

---
+++ print_quotient.py
@@ -0,0 +1,4 @@
+# Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+# Get user input for the first number.
+# Get user input for the second number.
+# Calculate the quotient of the two numbers and print the result.
