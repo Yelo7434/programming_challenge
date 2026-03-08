d62ba7dfdd3713c08f0ca97a74c7129c933dac3b
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-09 00:41:17 +0800

    print all numbers except the numbers ending 0

--- print_all_numbers.py
+++ print_all_numbers.py
@@ -1,3 +1,6 @@
 # Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
 # Loop through numbers from 0 to 100 and print the numbers that do not end with zero.
+for num in range(0, 101):
+    if num % 10 != 0:
+        print(num)
 # Print all numbers from 0 to 100 except those ending in zero.
