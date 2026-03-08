26d246d9ce1a0f0cbbe68d810a25ecad48b98e5b
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-09 00:15:01 +0800

    input two numbers

--- print_exponent.py
+++ print_exponent.py
@@ -1,4 +1,7 @@
 # Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+
 # Get user input for the first number.
+num1 = float(input("Enter the first number: "))
 # Get user input for the second number.
+num2 = float(input("Enter the second number: "))
 # Calculate the result of the first number raised to the second number and print the result.
