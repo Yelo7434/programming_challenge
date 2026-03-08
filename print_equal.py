53aa0e37ca084a9a2e0c80c85aa01f8f78e5c2f6
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-08 23:23:11 +0800

    input two numbers

--- print_equal.py
+++ print_equal.py
@@ -1,4 +1,7 @@
 # Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
 # Get user input for the first number.
+num1 = float(input("Enter the first number: "))
 # Get user input for the second number.
+num2 = float(input("Enter the second number: "))
 # Compare the two numbers and print "Equal" if they are the same.
