b0675ac9415fb5ca8f5fdf32bf2563e026a42070
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-09 00:14:09 +0800

    print exponent

---
+++ print_exponent.py
@@ -0,0 +1,4 @@
+# Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+# Get user input for the first number.
+# Get user input for the second number.
+# Calculate the result of the first number raised to the second number and print the result.
