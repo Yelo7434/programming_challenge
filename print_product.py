703f0522dd35aeb835185ae0f1743b3c5cb976a9
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-08 23:57:43 +0800

    input two numbers

--- print_product.py
+++ print_product.py
@@ -1,4 +1,7 @@
 # Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+
 # Get user input for the first number.
+num1 = float(input("Enter the first number: "))
 # Get user input for the second number.
+num2 = float(input("Enter the second number: "))
 # Calculate the product of the two numbers and print the result.
