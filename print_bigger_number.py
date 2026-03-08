166b6e2e0db557c4384ca0aa39368bff25c1f672
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-08 23:11:55 +0800

    Print bigger number

---
+++ print_bigger_number.py
@@ -0,0 +1,4 @@
+# Create a program that ask user to input 2 numbers. Print the bigger number.
+# Get user input for the first number.
+# Get user input for the second number.
+# Compare the two numbers and print the bigger one.
