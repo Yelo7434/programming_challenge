b52c6eac0dc58a203fe50de612ee881bc185414f
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-08 23:19:07 +0800

    compare the two numbers and print the bigger number

--- print_bigger_number.py
+++ print_bigger_number.py
@@ -5,4 +5,9 @@ num1 = float(input("Enter the first number: "))
 # Get user input for the second number.
 num2 = float(input("Enter the second number: "))
 # Compare the two numbers and print the bigger one.
-
+if num1 > num2:
+    print(f"The bigger number is: {num1}")
+elif num2 > num1:
+    print(f"The bigger number is: {num2}")
+else:
+    print("Both numbers are equal.")
