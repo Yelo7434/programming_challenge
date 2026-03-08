17adb0c4d22c0854ec1b0dc1839711849ecb7419
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-09 00:09:06 +0800

    input two numbers

--- print_quotient.py
+++ print_quotient.py
@@ -1,4 +1,7 @@
-# Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+# Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point.
+
 # Get user input for the first number.
+num1 = float(input("Enter the first number: "))
 # Get user input for the second number.
+num2 = float(input("Enter the second number: "))
 # Calculate the quotient of the two numbers and print the result.
