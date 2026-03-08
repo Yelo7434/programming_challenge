7420fdb264f29360915b65d2b09033f77d8ed2e8
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-08 23:22:00 +0800

    print equal

---
+++ print_equal.py
@@ -0,0 +1,4 @@
+# Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+# Get user input for the first number.
+# Get user input for the second number.
+# Compare the two numbers and print "Equal" if they are the same.
