a9e921ab58819880899831dcf7949e09f2ca3ebe
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-09 00:26:17 +0800

    print odd numbers

---
+++ print_odd_numbers.py
@@ -0,0 +1,5 @@
+# Create a program that ask user to input 10 numbers. Print how many are odd numbers.
+# Initialize a counter for odd numbers.
+# Get user input for 10 numbers and count how many are odd.
+# Loop through a range of 10 to get 10 numbers from the user.
+# Print the final count of odd numbers.
