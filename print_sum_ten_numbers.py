8858302d1ada4203f89f27fa0c6021031eee8ec1
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-09 00:21:19 +0800

    total sum

--- print_sum_ten_numbers.py
+++ print_sum_ten_numbers.py
@@ -1,5 +1,7 @@
-# Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+# Create a program that ask user to input 10 numbers. Print the sum of all the numbers.\
+
 # Initialize a variable to store the sum of the numbers.
+total_sum = 0
 # Get user input for 10 numbers and calculate the sum.
 # Loop through a range of 10 to get 10 numbers from the user.
 # Print the final sum of the numbers.
