6619eac20f63379a809b8f52a6c901e7fac3ab2b
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-08 23:17:20 +0800

    input 2 numbers

--- print_bigger_number.py
+++ print_bigger_number.py
@@ -1,4 +1,8 @@
 # Create a program that ask user to input 2 numbers. Print the bigger number.
+
 # Get user input for the first number.
+num1 = float(input("Enter the first number: "))
 # Get user input for the second number.
+num2 = float(input("Enter the second number: "))
 # Compare the two numbers and print the bigger one.
+
