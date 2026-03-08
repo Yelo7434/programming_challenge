63e9900a38a4aeb668c977eb634be893fbc90661
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-08 23:37:52 +0800

    print sum

---
+++ print_sum.py
@@ -0,0 +1,4 @@
+# Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+# Get user input for the first number.
+# Get user input for the second number.
+# Calculate the sum of the two numbers and print the result.
