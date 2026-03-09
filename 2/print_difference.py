18f94daae4f1ab40f6c49da49511128a5a23df16
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-09 23:08:22 +0800

    input two numbers to find difference

--- 2/print_difference.py
+++ 2/print_difference.py
@@ -1,3 +1,5 @@
 # Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
 # Get user input two numbers.
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))  
 # Get the difference of the two numbers and print it.
