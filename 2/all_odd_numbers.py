fcbb3938d8a11e206669cb324216561a25bd9cd4
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-10 00:17:01 +0800

    all odd numbers

---
+++ 2/all_odd_numbers.py
@@ -0,0 +1,3 @@
+# Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
+# Initialize a variable to keep track of the current number.
+# Loop through numbers from 0 to 100 and print the odd numbers.
