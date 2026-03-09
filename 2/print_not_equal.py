6ddd5a451883d323133a0a49db0511af036a811e
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-09 23:02:56 +0800

    input two numbers to recognize equal or not equal

--- 2/print_not_equal.py
+++ 2/print_not_equal.py
@@ -1,4 +1,6 @@
 # Create a program that asks the user to input 2 numbers. Print a message if they are not equal.
 # Get user input two numbers.
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
 # Convert the input to integers.
 # Compare the two numbers and print a message if they are not equal.
