1ac32c128b731df3283b6c78a16c8ed224887025
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-10 00:28:00 +0800

    print all numbers except ending 0 and 5

---
+++ 2/except_zero_and_five.py
@@ -0,0 +1,5 @@
+# Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
+# Loop through numbers from 0 to 100 and print the numbers that do not end in zero or five.
+# Use the modulus operator to check if the last digit of the number is not zero or five.
+# If the last digit is not zero or five, print the number.
+# Loop through numbers from 0 to 100 and print the numbers that do not end in zero or five.
