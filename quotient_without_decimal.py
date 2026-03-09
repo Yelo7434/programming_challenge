40f993062afafa1b0869c6baca94ff178793e083
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-09 23:12:57 +0800

    print the quotient of two numbers without decimal point.

---
+++ quotient_without_decimal.py
@@ -0,0 +1,3 @@
+# Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+# Get user input two numbers.
+# Calculate the quotient of the two numbers and print the result without the decimal point.
