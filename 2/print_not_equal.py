85b355b3aea260873fa8fcf4581add1a99228ddb
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-09 22:39:49 +0800

    print not equal

---
+++ 2/print_not_equal.py
@@ -0,0 +1,4 @@
+# Create a program that asks the user to input 2 numbers. Print a message if they are not equal.
+# Get user input two numbers.
+# Convert the input to integers.
+# Compare the two numbers and print a message if they are not equal.
