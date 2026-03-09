d2647b053aede2c9fa2422eec8adc2741af2270d
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-10 00:14:03 +0800

    count even numbers

---
+++ 2/count_even_numbers.py
@@ -0,0 +1,4 @@
+# Create a program that ask user to input 10 numbers. Print how many are even numbers.
+# Initialize a counter for even numbers.
+# Get user input for 10 numbers.
+# Convert the input to integers and store them in a list. Initialize an empty list to store the numbers.
