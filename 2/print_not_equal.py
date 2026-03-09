fd3d00e8d6cb312ae9edef9cb737aade7eba2a91
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-09 23:05:20 +0800

    compare two numbers then print not eual or equal

--- 2/print_not_equal.py
+++ 2/print_not_equal.py
@@ -1,6 +1,9 @@
 # Create a program that asks the user to input 2 numbers. Print a message if they are not equal.
 # Get user input two numbers.
-num1 = input("Enter the first number: ")
-num2 = input("Enter the second number: ")
-# Convert the input to integers.
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 # Compare the two numbers and print a message if they are not equal.
+if num1 != num2:
+    print("The numbers are not equal.")
+else:    
+    print("The numbers are equal.")
