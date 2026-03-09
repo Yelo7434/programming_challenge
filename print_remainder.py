1c34a34533cef92b66854b02c0c6c79bc60e8314
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-09 23:26:02 +0800

    print the remainder of the first number divided by the second.

---
+++ print_remainder.py
@@ -0,0 +1,3 @@
+# Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+# Get user input two numbers.
+# Compute the remainder of the division of the first number by the second number and print the result.
