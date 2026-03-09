bea4acf4f3632a1941c24d60279f243a14840e2f
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-09 23:26:30 +0800

    input two numbers

--- print_remainder.py
+++ print_remainder.py
@@ -1,3 +1,5 @@
 # Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
 # Get user input two numbers.
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 # Compute the remainder of the division of the first number by the second number and print the result.
