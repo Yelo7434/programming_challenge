853d8a133cbdebca4274ed21789b56cd51ef3475
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-10 00:05:34 +0800

    first number minus all 10 numbers

---
+++ num1_minus_all.py
@@ -0,0 +1,3 @@
+# Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+# Get user input for 10 numbers.
+# Convert the input to integers and store them in a list. Initialize an empty list to store the numbers.
