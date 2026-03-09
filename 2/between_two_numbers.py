e8627f27224e01577056aaa822425df311968315
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-10 00:32:54 +0800

    print the numbers in between

---
+++ 2/between_two_numbers.py
@@ -0,0 +1,3 @@
+# Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+# Get user input two numbers.
+# Compare the two numbers and print all the numbers between them.
