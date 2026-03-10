c10809da2da31be25e3badb26678ee04679bbfb9
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-10 22:05:35 +0800

    print unique numbers

--- 3/display_no_duplicates.py
+++ 3/display_no_duplicates.py
@@ -1,8 +1,14 @@
 # Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
-# Get user input 10 numbers.
-# Create an empty list to store the numbers.
+
 # Loop to get user input 10 numbers and store them in the list.
-# Create an empty list to store the numbers that don't have duplicate.
-# Loop through the list of numbers and check for duplicates.
-# If a number doesn't have duplicate, add it to the list of numbers that don't have duplicate.
+numbers = []
+for i in range(10):
+    num = int(input("Enter a number: "))
+    numbers.append(num)
+# Create an empty list to store the numbers.
+unique_numbers = []
+for num in numbers:
+    if numbers.count(num) == 1:
+        unique_numbers.append(num)
 # Print the list of numbers that don't have duplicate.
+print(unique_numbers)
