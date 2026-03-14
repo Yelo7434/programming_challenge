aa0d1febb10df6a55a69687274ed54aee56d8933
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-14 17:05:57 +0800

    Display lowest number

---
+++ 3/display_lowest_number.py
@@ -0,0 +1,6 @@
+# Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number.
+# Create an empty list to store the numbers.
+# Loop to get user input until the input is invalid.
+# If the input is a number, add it to the list.
+# If the input is invalid, break the loop.
+# After the loop, check if the list is empty. If it is empty, display "No numbers entered". If it is not empty, display the lowest number in the list.
