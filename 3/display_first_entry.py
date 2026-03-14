34b86727a9c4be5446d31d04f175669e084653e4
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-14 16:38:08 +0800

    Display all numbers and first number with duplicates

---
+++ 3/display_first_entry.py
@@ -0,0 +1,4 @@
+# Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+# Loop to get user input 10 numbers and store them in the list.
+# List to store numbers without duplicates
+# Display numbers that don't have duplicates
