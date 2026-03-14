3387dbd4d2dc49a65073f744a661bbfd4ef2932f
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-14 16:51:35 +0800

    Display all unique numbers

--- 3/display_first_duplicated_entry.py
+++ 3/display_first_duplicated_entry.py
@@ -1,4 +1,11 @@
 # Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+
 # Loop to get user input 10 numbers and store them in the list.
+numbers = [int(input("Enter a number: ")) for i in range(10)]
 # List to store numbers without duplicates
-# Display numbers that don't have duplicates
+result = []
+for num in numbers:
+    if num not in result:
+        result.append(num)
+# Display all numbers
+print("Numbers without duplicates:", result)
