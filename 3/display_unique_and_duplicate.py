3577967ea0c59456c9aef2bec2d01491de488798
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-14 16:56:42 +0800

    Display unique and duplicate

---
+++ 3/display_unique_and_duplicate.py
@@ -0,0 +1,6 @@
+# Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
+# Create an empty list to store the numbers.
+# Loop to get user input until the input is invalid.
+# If the input is a number, check if the number is in the list.
+# If the number is not in the list, add it to the list and display "Unique".
+# If the number is in the list, display "Duplicate".
