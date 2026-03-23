4a544c591840a01f77ea57d84102c6f7a219cc76
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-23 19:19:02 +0800

    Users will input a number until input is invalid, then the program will display the most duplicate number.

---
+++ 4/most_number_of_duplicates.py
@@ -0,0 +1,5 @@
+# Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
+# Create an empty list to store the numbers.
+# Loop to get user input until the input is invalid.
+# If the input is a number, add it to the list.
+# If the input is invalid, break the loop.
