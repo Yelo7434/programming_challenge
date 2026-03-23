c0c82d8932b3755f4e1533a2e31600f0bc70f2b2
unknown <hernandezrichtericefrancisco@gmail.com>
2026-03-23 19:22:10 +0800

    Final output

--- 4/most_number_of_duplicates.py
+++ 4/most_number_of_duplicates.py
@@ -4,3 +4,22 @@
 # If the input is a number, add it to the list.
 # If the input is invalid, break the loop.
 # After the loop, check if the list is empty. If it is empty, display "No numbers entered". If it is not empty, find the number with the most number of duplicate and display it.
+numbers = []
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        numbers.append(num)
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+        break
+if not numbers:
+    print("No numbers entered.")
+else:
+    most_duplicates = None
+    max_count = 0
+    for n in set(numbers):
+        count = numbers.count(n)
+        if count > max_count:
+            max_count = count
+            most_duplicates = n
+    print("Number with the most duplicates:", most_duplicates)
