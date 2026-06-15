f588aa3bfe42ad11b6ae796d10819d545d6fb655
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-15 17:49:24 +0800

    custom_swapcase

--- 6/without_swapcase.py
+++ 6/without_swapcase.py
@@ -1,1 +1,11 @@
 # swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
+def custom_swapcase(s):
+    result = ""
+    for char in s:
+        if 'a' <= char <= 'z':
+            result += chr(ord(char) - 32)
+        elif 'A' <= char <= 'Z':
+            result += chr(ord(char) + 32)
+        else:
+            result += char
+    return result
