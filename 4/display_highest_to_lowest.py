e47046408fa0aca3168b490780c47d744d888f06
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-15 16:38:06 +0800

    While true loop then number.sort(reverse=true)

--- 4/display_highest_to_lowest.py
+++ 4/display_highest_to_lowest.py
@@ -1,1 +1,9 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
+numbers = []
+while True:
+    user_input = input("Enter a number: ")
+    if not user_input.isdigit():
+        break
+    numbers.append(int(user_input))
+numbers.sort(reverse=True)
+print(numbers)
