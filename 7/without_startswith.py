9f69f6d34880f72712053bfb696c46da8aa79402
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-15 18:29:05 +0800

    custom_startswith

--- 7/without_startswith.py
+++ 7/without_startswith.py
@@ -1,1 +1,3 @@
 # startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
+def custom_startswith(s, prefix):
+    return s[:len(prefix)] == prefix
