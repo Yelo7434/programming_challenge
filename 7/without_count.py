ed24fc582347fb5e969adb4e5f7a6ffcd2808227
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-15 18:34:37 +0800

    custom_count

--- 7/without_count.py
+++ 7/without_count.py
@@ -1,1 +1,8 @@
 # count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
+def custom_count(s, sub):
+    count = 0
+    sub_len = len(sub)
+    for i in range(len(s) - sub_len + 1):
+        if s[i:i + sub_len] == sub:
+            count += 1
+    return count
