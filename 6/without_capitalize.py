e04bd19fb643671b5f992d82b675fd9de900d9cf
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-15 17:58:41 +0800

    custom_capitalize

--- 6/without_capitalize.py
+++ 6/without_capitalize.py
@@ -1,1 +1,5 @@
 # capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
+def custom_capitalize(s):
+    if not s: return s
+    return s[0].upper() + s[1:].lower()
+    
