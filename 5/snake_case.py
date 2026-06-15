045e8c8c394df2ff2575f4f8d1b8e341c1dd2587
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-15 17:18:00 +0800

    _.join, lower(), and split()

--- 5/snake_case.py
+++ 5/snake_case.py
@@ -1,1 +1,4 @@
 # Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
+fullname = input("Enter fullname: ")
+# Convert to lower, split by spaces, and join with underscores
+print("_".join(fullname.lower().split()))
