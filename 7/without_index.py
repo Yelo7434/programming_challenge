971f85000ff1a48050da2a729e45faa9d2327f19
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-15 18:36:22 +0800

    custom_index

--- 7/without_index.py
+++ 7/without_index.py
@@ -1,1 +1,7 @@
 # index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
+def custom_index(s, sub):
+    sub_len = len(sub)
+    for i in range(len(s) - sub_len + 1):
+        if s[i:i + sub_len] == sub:
+            return i
+    raise ValueError("substring not found")
