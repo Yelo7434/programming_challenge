e1c5b86b655b5d777c83c6d7813a8ed87b6e8b0c
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-15 17:40:04 +0800

    custom_isupper

--- 6/without_isupper.py
+++ 6/without_isupper.py
@@ -1,1 +1,9 @@
 # isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
+def custom_isupper(s):
+    has_alpha = False
+    for char in s:
+        if 'a' <= char <= 'z':
+            return False
+        if 'A' <= char <= 'Z':
+            has_alpha = True
+    return has_alpha
