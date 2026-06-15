05d8443a5c72e074db90372efb04dbe59ed890eb
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-15 17:45:13 +0800

    custom_ljust

--- 6/without_ljust.py
+++ 6/without_ljust.py
@@ -1,1 +1,5 @@
 # ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
+def custom_ljust(s, width):
+    if len(s) >= width:
+        return s
+    return s + (' ' * (width - len(s)))
