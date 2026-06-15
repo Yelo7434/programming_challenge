6be4ff9092d35fc1a6bf2e2b2f54ce7ff061043f
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-15 17:32:54 +0800

    custom_lstrip

--- 6/without_lstrip.py
+++ 6/without_lstrip.py
@@ -1,1 +1,6 @@
 # lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
+def custom_lstrip(s):
+    i = 0
+    while i < len(s) and s[i] == ' ':
+        i += 1
+    return s[i:]
