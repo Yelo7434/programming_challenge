3ff83ef7f65cfbe0e95ae6c8c5118a102553ec68
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-15 18:27:05 +0800

    custom_islower

--- 7/without_islower.py
+++ 7/without_islower.py
@@ -1,1 +1,9 @@
 # islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
+def custom_islower(s):
+    has_alpha = False
+    for char in s:
+        if 'A' <= char <= 'Z':
+            return False
+        if 'a' <= char <= 'z':
+            has_alpha = True
+    return has_alpha
