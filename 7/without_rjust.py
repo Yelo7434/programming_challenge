80cd1e2a37c5237dca3cc54db524c641d507965f
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-15 18:30:57 +0800

    custom_rjust

--- 7/without_rjust.py
+++ 7/without_rjust.py
@@ -1,1 +1,5 @@
 # rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
+def custom_rjust(s, width):
+    if len(s) >= width:
+        return s
+    return (' ' * (width - len(s))) + s
