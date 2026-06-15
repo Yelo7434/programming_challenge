08d68b1e77d21ff4bd2722da8f7104496faeaaa1
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-15 17:50:53 +0800

    Make first letter to capital letter without using capitalize() fuction

---
+++ 6/without_capitalize.py
@@ -0,0 +1,1 @@
+# capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
