275cc52e5338f2b47e1719f3846b88370c5ba7e5
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-15 18:23:36 +0800

    custom_removesuffix

--- 7/without_removesuffix.py
+++ 7/without_removesuffix.py
@@ -1,1 +1,5 @@
 # removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
+def custom_removesuffix(s, suffix):
+    if s.endswith(suffix): # Assuming standard endswith is allowed; otherwise use s[-len(suffix):] == suffix
+        return s[:-len(suffix)]
+    return s
