ff42b255edf8404b059c7e7749f32a462a7480b7
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-15 18:11:28 +0800

    custom_rstrip

--- 7/without_rstrip.py
+++ 7/without_rstrip.py
@@ -1,1 +1,6 @@
 # rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
+def custom_rstrip(s):
+    i = len(s) - 1
+    while i >= 0 and s[i] == ' ':
+        i -= 1
+    return s[:i+1]
