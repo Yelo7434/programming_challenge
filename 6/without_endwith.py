fe4264a9a85b8cd7f6dfe4a71354a33f39c0cf38
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-15 17:42:21 +0800

    custom_endwith

--- 6/without_endwith.py
+++ 6/without_endwith.py
@@ -1,1 +1,3 @@
 # endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+def custom_endswith(s, suffix):
+    return s[-len(suffix):] == suffix
