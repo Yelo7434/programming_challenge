aeae19be1490059be1cf2c9e3739b43105d346df
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-15 17:37:20 +0800

    custom_lower

--- 6/without_lower.py
+++ 6/without_lower.py
@@ -1,1 +1,9 @@
 # lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+def custom_lower(s):
+    result = ""
+    for char in s:
+        if 'A' <= char <= 'Z':
+            result += chr(ord(char) + 32)
+        else:
+            result += char
+    return result
