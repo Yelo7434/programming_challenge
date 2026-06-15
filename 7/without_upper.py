093a3f069587d8b5843f810070920955893ae6d8
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-15 18:25:28 +0800

    custom_upper

--- 7/without_upper.py
+++ 7/without_upper.py
@@ -1,1 +1,9 @@
 # upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
+def custom_upper(s):
+    result = ""
+    for char in s:
+        if 'a' <= char <= 'z':
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    return result
