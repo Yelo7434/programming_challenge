6bae5188cc57160a801ec7d4fbe6a51f8a87cdb2
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-15 17:10:33 +0800

    split()

--- 5/number_of_words.py
+++ 5/number_of_words.py
@@ -1,1 +1,4 @@
 # Create a program that ask the user to input a complete statement. Print the number of words in the input.
+statement = input("Enter a statement: ")
+words = statement.split()
+print(len(words))
