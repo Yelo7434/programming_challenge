2c77c64625845faac28cf6a6b86dc7415ed889ed
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-15 17:47:36 +0800

    custom_center

--- 6/without_center.py
+++ 6/without_center.py
@@ -1,1 +1,6 @@
 # center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
+def custom_center(s, width):
+    if len(s) >= width:
+        return s
+    pad = (width - len(s)) // 2
+    return (' ' * pad) + s + (' ' * (width - len(s) - pad))
