51bec14c0cae542c928f37d3a52983510e988b93
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-15 17:08:48 +0800

    swapcase()

--- 5/reverse_casing.py
+++ 5/reverse_casing.py
@@ -1,1 +1,3 @@
 # Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+fullname = input("Enter fullname: ")
+print(fullname.swapcase())
