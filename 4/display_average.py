acc89ca1f84c2415126700e8ee3bbbd936f86940
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-15 16:41:20 +0800

    while true loop and if numbers to get average

--- 4/display_average.py
+++ 4/display_average.py
@@ -1,1 +1,9 @@
 # Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
+numbers = []
+while True:
+    user_input = input("Enter a number: ")
+    if not user_input.isdigit():
+        break
+    numbers.append(int(user_input))
+if numbers:
+    print(f"Average: {sum(numbers) / len(numbers)}")
