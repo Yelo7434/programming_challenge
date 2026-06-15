a51e790ea6fed5c2ec7ebb8b4cb3ac5145f567ff
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-15 16:45:42 +0800

    lstrip()

--- 5/without_space.py
+++ 5/without_space.py
@@ -1,1 +1,3 @@
 # Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
+fullname = input("Enter fullname: ")
+print(fullname.lstrip())
