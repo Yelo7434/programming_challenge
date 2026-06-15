b66ad42e535266ee9b357662546cd7d32176a0b8
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-15 18:38:12 +0800

    custom_rindex

--- 7/without_rindex.py
+++ 7/without_rindex.py
@@ -1,1 +1,7 @@
-# rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() fun
+# rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
+def custom_rindex(s, sub):
+    sub_len = len(sub)
+    for i in range(len(s) - sub_len, -1, -1):
+        if s[i:i + sub_len] == sub:
+            return i
+    raise ValueError("substring not found")
