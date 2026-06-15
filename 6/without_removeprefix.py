9172cfc946990134237384a9623132ae485f5348
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-15 17:35:26 +0800

    custom_removeprefix

--- 6/without_removeprefix.py
+++ 6/without_removeprefix.py
@@ -1,1 +1,5 @@
 # removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
+def custom_removeprefix(s, prefix):
+    if s[:len(prefix)] == prefix:
+        return s[len(prefix):]
+    return s
